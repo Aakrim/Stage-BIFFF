856ac9d613e61bb4f82ea0042debccc438aab2a3
emzakh <massimino.gutierrezmantione@gmail.com>
2021-03-10 18:37:05 +0100

    debut ajout insertion auto image

--- themes/twentytwentyone-child/functions.php
+++ themes/twentytwentyone-child/functions.php
@@ -228,7 +228,49 @@ add_action("admin_init", function () {
 		
 		//DELETE FROM `wp_posts` WHERE `wp_posts`.`ID` > 300;	
 				
-			
+						// Add Featured Image to Post
+		$image_url        = $post["images"]; // Define the image URL here
+		$image_name       = preg_replace( '/[^a-z0-9]/i', '', $post["post_name"])."_poster.jpg";
+		$upload_dir       = wp_upload_dir(); // Set upload folder
+		$image_data       = file_get_contents($image_url); // Get image data
+		$unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
+		$filename         = basename( $unique_file_name ); // Create image file name
+
+		// Check folder permission and define file location
+		if( wp_mkdir_p( $upload_dir['path'] ) ) {
+			$file = $upload_dir['path'] . '/' . $filename;
+		} else {
+			$file = $upload_dir['basedir'] . '/' . $filename;
+		}
+
+		// Create the image  file on the server
+		file_put_contents( $file, $image_data );
+
+		// Check image file type
+		$wp_filetype = wp_check_filetype( $filename, null );
+
+		// Set attachment data
+		$attachment = array(
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title'     => sanitize_file_name( $filename ),
+			'post_content'   => '',
+			'post_status'    => 'inherit'
+		);
+
+		// Create the attachment
+		$attach_id = wp_insert_attachment( $attachment, $file, $post["ID"] );
+
+		// Include image.php
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+		// Define attachment metadata
+		$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+
+		// Assign metadata to attachment
+		wp_update_attachment_metadata( $attach_id, $attach_data );
+
+		// And finally assign featured image to post
+		set_post_thumbnail( $post["ID"], $attach_id );		
 			
 
 		// Insertion du post dans la database
