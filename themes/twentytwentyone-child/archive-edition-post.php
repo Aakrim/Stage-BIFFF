13680830de26a477152d2047cc687f56d53a504a
emzakh <massimino.gutierrezmantione@gmail.com>
2021-03-31 19:27:40 +0200

    ligne du temps màj

--- themes/twentytwentyone-child/archive-edition-post.php
+++ themes/twentytwentyone-child/archive-edition-post.php
@@ -1,48 +1,55 @@
+
 <!-- http://localhost/wikibifff/edition-post/  -->
 
 <?php get_header() ?>
-<?php 
-      $term = get_queried_object();
+<?php
+$term = get_queried_object();
 
-      $children = get_terms( 'category', array(
-          'annee'    => $term->term_id,
-          'hide_empty' => false
-      ) );
-  
-   //  echo '<li>'. $children .'</li>';
-   //var_dump($children);
+$children = get_terms('category', array(
+    'annee'    => $term->term_id,
+    'hide_empty' => false
+
+));
 
 ?>
 
 <div class="container align-items-center">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>      
-   
+    <?php $i = 1; ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); $i++;  ?>
+
+    <?php if ($i % 2 == 0) { ?>
             <div class="row p-5">
-                <div class="col-sm content">   
-                    <?php 
-                      if(intval(the_terms( get_the_ID() , 'category')) % 2 === 0)  {
-                          ?>
-                    <div class="boxLeft mb-5"><h4 class="fst-italic"><?php the_field('nom'); ?></h4> </div>
+                    <div class="col-sm content">
+                        <div class="boxLeft mb-5">
+                            <h4 class="fst-italic"><?php the_field('nom'); ?></h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-auto">
+                        <div class="timeline-bar"></div>
+                        <div class="years">
+                            <?php the_terms(get_the_ID(), 'category'); ?>
+                        </div>
+
                     <?php } ?>
 
-                </div>
-                <div class="col-sm-auto">
-                    <div class="timeline-bar"></div>
-                    <div class="years"><?php the_terms( get_the_ID() , 'category'); ?></div>
-                    <div class="years"><?php the_terms( get_the_ID() , 'category'); ?></div>
-                </div>  
-                <div class="col-sm content">
-                <?php 
-                      if( intval(the_terms( get_the_ID() , 'category')) % 2 !== 0)  {
-                          ?>
-                    <div class="boxLeft mb-5"><h4 class="fst-italic"><?php the_field('nom'); ?></h4> </div>
-                    <?php } ?>
-                   
-                </div>
+                    <?php if ($i % 2 !== 0) { ?>
+
+                        <div class="years">
+                            <?php the_terms(get_the_ID(), 'category'); ?>
+                        </div>
+                    </div>
+
+
+                    <div class="col-sm content">
+                        <div class="boxLeft mb-5">
+                            <h4 class="fst-italic"><?php the_field('nom'); ?></h4>
+                        </div>
+                    </div>
             </div>
+        <?php }  ?>
 
-        <?php endwhile; ?>
-    <?php endif ?>
+    <?php endwhile; ?>
+<?php endif ?>
 </div>
 
 
